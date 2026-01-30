1c9985bc4e091db46acf663b8ee9d8104af45123
ericocneto <cordeironetoerico@gmail.com>
2026-01-30 03:24:30 -0300

    feat: add a real switch player feature

--- src/components/ClockContainer.tsx
+++ src/components/ClockContainer.tsx
@@ -4,8 +4,15 @@ import { useEffect, useState } from "react";
 
 export default function ClockContainer() {
   const [playing, setPlaying] = useState<"white" | "black">("white");
+  useEffect(() => {
+    if (playing === "white") {
+      console.log("tempo do branco");
+    } else {
+      console.log("tempo do preto");
+    }
+  }, [playing]);
   return (
-    <div className="gap-8 flex flex-col">
+    <div className="flex gap-8">
       <WhiteDisplay playing={playing} setPlaying={setPlaying} />
       <BlackDisplay playing={playing} setPlaying={setPlaying} />
     </div>
--- src/components/WhiteDisplay.tsx
+++ src/components/WhiteDisplay.tsx
@@ -3,12 +3,23 @@ type Props = {
   setPlaying: React.Dispatch<React.SetStateAction<"white" | "black">>;
 };
 export default function WhiteDisplay({ playing, setPlaying }: Props) {
-  return (
-    <div
-      onClick={() => setPlaying(playing === "white" ? "black" : "white")}
-      className="flex flex-col items-center justify-center w-72 h-72 bg-neutral-800/40 rounded-2xl cursor-pointer"
-    >
-      <span className="text-5xl  font-bold text-white">00:00</span>
-    </div>
-  );
+  if (playing === "white") {
+    return (
+      <div
+        onClick={() => setPlaying(playing === "white" ? "black" : "white")}
+        className="flex flex-col items-center justify-center w-80 h-80 bg-neutral-800/40 rounded-2xl cursor-pointer"
+      >
+        <span className="text-5xl  font-bold text-white">00:00</span>
+      </div>
+    );
+  } else if (playing === "black") {
+    return (
+      <div
+        onClick={() => setPlaying(playing === "white" ? "black" : "white")}
+        className="flex flex-col items-center justify-center w-72 h-72 bg-neutral-800/40 rounded-2xl cursor-pointer"
+      >
+        <span className="text-5xl  font-bold text-white">00:00</span>
+      </div>
+    );
+  }
 }
