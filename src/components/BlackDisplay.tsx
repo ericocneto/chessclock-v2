8e3f618105949d2ba37821c5130510e92ffc32a3
ericocneto <cordeironetoerico@gmail.com>
2026-01-30 00:47:58 -0300

    fix: fix cursor pointer in black display

--- src/components/BlackDisplay.tsx
+++ src/components/BlackDisplay.tsx
@@ -1,9 +1,7 @@
 export default function BlackDisplay() {
   return (
-    <div className="flex flex-col items-center justify-center w-72 h-72 bg-neutral-800/70 rounded-2xl">
-      <span className="text-5xl cursor-pointer font-bold text-white">
-        00:00
-      </span>
+    <div className="flex flex-col items-center justify-center w-72 h-72 bg-neutral-800/70 rounded-2xl cursor-pointer">
+      <span className="text-5xl  font-bold text-white">00:00</span>
     </div>
   );
 }
